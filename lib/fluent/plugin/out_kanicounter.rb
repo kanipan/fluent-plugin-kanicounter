15ac128d35b4ea4a5c2e52bf560e07efa41de97c
kanipan <kanipan@www4087uf.sakura.ne.jp>
2012-06-15 18:11:15 +0900

    init three

--- lib/fluent/plugin/out_kanicounter.rb
+++ lib/fluent/plugin/out_kanicounter.rb
@@ -1,7 +1,40 @@
-#require "fluent-plugin-kanicounter/version"
+class Fluent::KaniCounterOutput < Fluent::BufferedOutput
+  Fluent::Plugin.register_output('kanicounter', self)
 
-module Fluent
-    module Kanicounter < Fluent::BuffereOutput
-      # Your code goes here...
-    end
+  # config_param :hoge, :string, :default => 'hoge'
+  config_param :tag, :string
+  config_param :value, :integer :default => 0
+
+  def initialize
+    super
+    # require 'hogepos'
+  end
+
+  def configure(conf)
+    super
+    # @path = conf['path']
+  end
+
+  def start
+    super
+    # init
+  end
+
+  def shutdown
+    super
+    # destroy
+  end
+
+  def format(tag, time, record)
+    [tag, time, record].to_msgpack
+  end
+
+  def write(chunk)
+    records = []
+    chunk.msgpack_each { |record|
+       record = "kani"
+       records << record
+    }
+    # write records
+  end
 end
